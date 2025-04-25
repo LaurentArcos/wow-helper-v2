bcc295e929fe8bba3612a877de984e3868f1fd5e
LaurentArcos <laurent.arcos@gmail.com>
2025-04-25 23:53:42 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Tailwind!
+      </h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Ton setup est fonctionnel 🎉
+      </p>
+    </div>
+  );
+}
