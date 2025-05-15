abebe655cf813cfadd2911b5c570fb6851fb11f3
LaurentArcos <laurent.arcos@gmail.com>
2025-05-15 21:02:10 +0200

    app simplfié

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import Home from './pages/Home';
-import { useEffect, useState } from 'react';
-import { fetchSheetData } from './utils/fetchSheetData';
-import { Character } from './types/Character';
 
 export default function App() {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(null);
 
-useEffect(() => {
-  const fetchData = async () => {
-    const data = await fetchSheetData<Character>('personnages'); 
-    setCharacters(data);
-    setSelectedCharacter(data[0]);
-  };
-  fetchData();
-}, []);
 
   return (
     <Home />
